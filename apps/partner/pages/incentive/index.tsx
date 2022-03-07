858b191c833a9bc817db7276617bee7fbaa69d37
OlaStenberg <hello@olastenberg.com>
2022-03-07 09:33:59 +0100

    feat: init partner portal

---
+++ apps/partner/pages/incentive/index.tsx
@@ -0,0 +1,7 @@
+export default function Incentive() {
+  return (
+    <div>
+      <h1>Create Incentive</h1>
+    </div>
+  )
+}
---
+++ apps/partner/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Partner() {
+  return (
+    <div>
+      <h1>Partner Portal</h1>
+    </div>
+  )
+}
