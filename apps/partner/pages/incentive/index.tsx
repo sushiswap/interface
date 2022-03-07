71cb5ad2332c1ec08de90a9008554ba69dfb06d6
OlaStenberg <hello@olastenberg.com>
2022-03-07 12:20:26 +0100

    feat: call contract

--- apps/partner/pages/incentive/index.tsx
+++ apps/partner/pages/incentive/index.tsx
@@ -6,7 +6,6 @@ import { Web3ReactProvider } from '@web3-react/core'
 import { useEffect, useState } from 'react'
 import { STAKING_ADDRESS, useStakingContract } from '../../hooks/useContract'
 
-
 const DEFAULT_END_DATE = new Date()
 DEFAULT_END_DATE.setDate(DEFAULT_END_DATE.getDate() + 7)
 
@@ -16,9 +15,8 @@ export default function Incentive() {
 
   const [tokenAddress, setTokenAddress] = useState<string>('0xc4cbede6c5cc7d0c775adfc76803c5888c1530f0')
   const [rewardTokenAddress, setRewardTokenAddress] = useState<string>('0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa')
-  const [amount, setAmount] = useState<string>('')
+  const [amount, setAmount] = useState<string>('123')
   const [duration, setDuration] = useState<number>(28)
-
 
   useEffect(() => {
     ;(async function () {
@@ -32,14 +30,7 @@ export default function Incentive() {
     })()
   }, [chainId])
 
-  const connect = () =>
-    activate(
-      new InjectedConnector({}),
-      (err) => {
-        console.log(err)
-      },
-      true,
-    )
+  const connect = () => activate(new InjectedConnector({}))
 
   function toUnix(date: Date): string {
     return (date.getTime() / 1000).toFixed().toString()
@@ -47,54 +38,54 @@ export default function Incentive() {
 
   const onSubmit = async () => {
     if (!account) {
-      connect();
-      console.log('submit')
-      return;
+      connect()
     }
+    
+    let endDate = new Date()
+    endDate.setDate(endDate.getDate() + duration)
+  
+    await stakingContract.createIncentive(tokenAddress, rewardTokenAddress, amount, toUnix(new Date()), toUnix(endDate))
+    // validation
+    // TODO: handle allowance, await token and rewardToken .approve(token, BigInt(1000e18))
+    // throw exceptions? 
   }
 
   return (
+    <div>
+      <h1>Create Incentive</h1>
+      <button className="p-4 m-4 border cursor-pointer" onClick={connect}>
+        {!account ? 'Connect Wallet' : `${account?.slice(0, 5)}...${account?.slice(-4)}`}
+      </button>
+
       <div>
-        <h1>Create Incentive</h1>
-        <button className="p-4 m-4 border cursor-pointer" onClick={connect}>
-          {!account ? 'Connect Wallet' : `${account?.slice(0, 5)}...${account?.slice(-4)}`}
-        </button>
+        <div>
+          <label>Token address: </label>
+          <br />
+          <input onChange={(e) => setTokenAddress(e.target.value)} placeholder="0x6.."></input>
+        </div>
 
         <div>
-          <div>
-            <label>Token address: </label>
-            <br />
-            <input
-              onChange={(e) => setTokenAddress(e.target.value)}
-              placeholder="0x6.."
-            ></input>
-          </div>
+          <label>Token Reward address: </label>
+          <br />
+          <input onChange={(e) => setRewardTokenAddress(e.target.value)} placeholder="0x6B.." />
+        </div>
 
-          <div>
-            <label>Token Reward address: </label>
-            <br />
-            <input
-              onChange={(e) => setRewardTokenAddress(e.target.value)}
-              placeholder="0x6B.."
-            />
-          </div>
+        <div>
+          <label>Amount: </label>
+          <br />
+          <input onChange={(e) => setAmount(e.target.value)} placeholder="10.235134" />
+        </div>
 
-          <div>
-            <label>Amount: </label>
-            <br />
-            <input onChange={(e) => setAmount(e.target.value)} placeholder="10.235134" />
-          </div>
+        <div>
+          <label>Duration (days): </label>
+          <br />
+          <input onChange={(e) => setDuration(e.target.valueAsNumber)} placeholder={`${duration}`} />
+        </div>
 
-          <div>
-            <label>Duration (days): </label>
-            <br />
-            <input onChange={(e) => setDuration(e.target.valueAsNumber)} placeholder={`${duration}`} />
-          </div>
-
-          <div>
-            <button onClick={onSubmit}>Submit</button>
-          </div>
+        <div>
+          <button onClick={onSubmit}>Submit</button>
         </div>
       </div>
+    </div>
   )
 }
