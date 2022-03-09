ec9bb063ab3cb1bb2e0e049839dc4398203bb2c0
OlaStenberg <hello@olastenberg.com>
2022-03-09 09:57:09 +0100

    refactor: add SwapAssetPanel

--- apps/partner/pages/incentive/index.tsx
+++ apps/partner/pages/incentive/index.tsx
@@ -1,157 +1,58 @@
 import { useWeb3React } from '@web3-react/core'
-import { InjectedConnector } from '@web3-react/injected-connector'
-import { Web3Provider } from '@ethersproject/providers'
-import { Web3ReactProvider } from '@web3-react/core'
+import { useStakingContract } from '../../hooks/useContract'
 
-import { useEffect, useState } from 'react'
-import { STAKING_ADDRESS, useERC20, useStakingContract } from '../../hooks/useContract'
-import { Contract } from '@ethersproject/contracts'
-import ERC20_ABI from '../../abis/ERC20.json'
-import { useToken } from '../../hooks/useToken'
-import {BigintIsh} from 'math'
-
-const DEFAULT_END_DATE = new Date()
-DEFAULT_END_DATE.setDate(DEFAULT_END_DATE.getDate() + 7)
 
 export default function Incentive() {
   const { account, activate, library, chainId } = useWeb3React()
   const stakingContract = useStakingContract()
 
-  const [tokenAddress, setTokenAddress] = useState<string>('') //0xc4cbede6c5cc7d0c775adfc76803c5888c1530f0
-  const token = useToken(chainId, tokenAddress)
-  const [tokenApproved, setTokenApproved] = useState<boolean>(false)
-  const [tokenAllowance, setTokenAllowance] = useState<BigintIsh>(0) //0xc4cbede6c5cc7d0c775adfc76803c5888c1530f0
-
-  // const tokenContract = useERC20('')
-  const [rewardTokenAddress, setRewardTokenAddress] = useState<string>('') //0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa
-  // const rewardTokenContract = useERC20('')
-  const [rewardTokenApproved, setRewardTokenApproved] = useState<boolean>(false)
-
-  const [amount, setAmount] = useState<number>(123)
-  const [duration, setDuration] = useState<number>(28)
-  const [inputError, setInputError] = useState<string>('Submit')
-
-  useEffect(() => {
-    ;(async function () {
-      if (chainId !== 42) {
-        await library?.provider.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: '0x42' }],
-        })
-      }
-    })()
-  }, [chainId])
-
-  useEffect(() => {
-    validate()
-  }, [])
-
-  const validate = () => {
-    if (!account) {
-      connect()
-    }
-    validateToken(tokenAddress, 'Token')
-    validateToken(rewardTokenAddress, 'Reward Token')
-  }
-
-  const validateToken = async (tokenAddress: string, tokenType: string) => {
-    if (tokenAddress !== '') {
-      let isValid = (await token).decimals !== null
-      if (isValid) {
-        let erc20Contract = new Contract(tokenAddress, ERC20_ABI, account ? library.getSigner(account) : library)
-
-        // try {
-          // populate token object
-          let allowance = (await erc20Contract.allowance(account, STAKING_ADDRESS))
-          // let amountDecimals = amount
-          setTokenAllowance(allowance)
-          if (allowance > amount) {
-            console.log("All good, no need to approve.")
-          } else {
-            console.log("TokenAllowance too low, need to approve")
-
-            await erc20Contract.approve(STAKING_ADDRESS, 10^((await token).decimals)).wait(() => {
-            setInputError(`Approve ${tokenType}`)
-            setTokenApproved(true)
-          })
-          }
-          // console.log({ tokenAllowance, amount })
-          // TODO: approveToken Button render instead?
-          // await (
-          //   await erc20Contract.approve(STAKING_ADDRESS, BigInt(1000e18))
-          // ).wait(() => {
-          //   setInputError(`Approve ${tokenType}`)
-          //   setTokenApproved(true)
-          // })
-        // } catch (error) {
-        //   setInputError(`Invalid ${tokenType} Address`)
-        // }
-      }
-      
-    }
-  }
-
-  const connect = () => activate(new InjectedConnector({}))
-
-  function toUnix(date: Date): string {
-    return (date.getTime() / 1000).toFixed().toString()
-  }
-
-  const onSubmit = async () => {
-    if (!account) {
-      connect()
-    }
-
-    validate()
-    let endDate = new Date()
-    endDate.setDate(endDate.getDate() + duration)
-
-    await stakingContract.createIncentive(tokenAddress, rewardTokenAddress, amount, toUnix(new Date()), toUnix(endDate))
-    // validation
-    // TODO: handle allowance, await token and rewardToken .approve(token, BigInt(1000e18))
-    // throw exceptions?
-  }
 
   return (
     <div>
       <h1>Create Incentive</h1>
-      <button className="p-4 m-4 border cursor-pointer" onClick={connect}>
-        {!account ? 'Connect Wallet' : `${account?.slice(0, 5)}...${account?.slice(-4)}`}
-      </button>
-
-      <div>
-        <div>
-          <label>Token address: </label>
-          SLP 0xc4cbede6c5cc7d0c775adfc76803c5888c1530f0
-          MEOW 0x83D0346Ff4E2fc634C91050973e3AE54b3a95615
-          <br />
-          <input onChange={(e) => setTokenAddress(e.target.value)} placeholder="0x6.."></input>
-        </div>
-
-        <div>
-          <label>Token Reward address: </label>
-          <br />
-          <input onChange={(e) => setRewardTokenAddress(e.target.value)} placeholder="0x6B.." />
-        </div>
-
-        <div>
-          <label>Amount: </label>
-          <br />
-          <input onChange={(e) => setAmount(e.target.value)} placeholder="10.235134" />
-        </div>
-
-        <div>
-          <label>Duration (days): </label>
-          <br />
-          <input onChange={(e) => setDuration(e.target.valueAsNumber)} placeholder={`${duration}`} />
-        </div>
-
-        <div>
-          <button onClick={onSubmit} disabled={inputError !== null}>
-            {inputError === null ? 'Sumbit' : inputError}
-          </button>
-        </div>
-      </div>
+      <div className="flex flex-col gap-3">
+          <SwapAssetPanel
+            spendFromWallet={true}
+            header={(props) => (
+              <SwapAssetPanel.Header
+                {...props}
+                label={
+                  independentField === Field.OUTPUT && !showWrap ? i18n._(t`Swap from (est.):`) : i18n._(t`Swap from:`)
+                }
+              />
+            )}
+            currency={currencies[Field.INPUT]}
+            value={formattedAmounts[Field.INPUT]}
+            onChange={handleTypeInput}
+            onSelect={handleInputSelect}
+          />
+          <div className="z-0 flex justify-center -mt-6 -mb-6">
+            <div
+              role="button"
+              className="p-1.5 rounded-full bg-dark-800 border shadow-md border-dark-700 hover:border-dark-600"
+              onClick={() => {
+                setApprovalSubmitted(false) // reset 2 step UI for approvals
+                onSwitchTokens()
+              }}
+            >
+              <ArrowDownIcon width={14} className="text-high-emphesis hover:text-white" />
+            </div>
+          </div>
+          <SwapAssetPanel
+            spendFromWallet={true}
+            header={(props) => (
+              <SwapAssetPanel.Header
+                {...props}
+                label={independentField === Field.INPUT && !showWrap ? i18n._(t`Swap to (est.):`) : i18n._(t`Swap to:`)}
+              />
+            )}
+            currency={currencies[Field.OUTPUT]}
+            value={formattedAmounts[Field.OUTPUT]}
+            onChange={handleTypeOutput}
+            onSelect={handleOutputSelect}
+            priceImpact={priceImpact}
+            priceImpactCss={priceImpactCss}
+          />
     </div>
   )
 }
