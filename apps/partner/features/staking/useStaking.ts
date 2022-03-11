2c4ebaced912bf63938d8cffc91f3dc2a8699c57
OlaStenberg <hello@olastenberg.com>
2022-03-11 20:34:58 +0100

    feat: staking contract hook, wire up inputs

---
+++ apps/partner/features/staking/useStaking.ts
@@ -0,0 +1,30 @@
+import { useCallback } from 'react'
+import { hooks } from '../../components/connectors/metaMask'
+import { useStakingContract } from '../../hooks/useContract'
+import { useTransactionAdder } from '../../state/transactions/hooks'
+
+const useStaking = () => {
+  const account = hooks.useAccount()
+  const addTransaction = useTransactionAdder()
+  const contract = useStakingContract(true)
+
+  const createIncentive = useCallback(
+    async (poolAddres: string, rewardTokenAddress: string, amount: number, startDate: number, endDate: number) => {
+      if (!contract) throw new Error('Contract not initialized')
+      if (!account) throw new Error('Wallet not connected')
+
+      const tx = await contract.createIncentive(poolAddres, rewardTokenAddress, amount, startDate, endDate)
+
+      addTransaction(tx, { summary: 'Initialize Incentive creation' })
+
+      return tx
+    },
+    [account, contract, addTransaction],
+  )
+
+  return {
+    createIncentive,
+  }
+}
+
+export default useStaking
--- apps/partner/pages/incentive/index.tsx
+++ apps/partner/pages/incentive/index.tsx
@@ -2,11 +2,10 @@ import { Token } from 'currency'
 import { useMemo, useState } from 'react'
 import NoSSR from 'react-no-ssr'
 import MetaMaskCard from '../../components/connectors/MetaMaskCard'
+import useStaking from '../../features/staking/useStaking'
 import { useTokenComparator } from '../../functions/sorting'
 import { useAllTokens } from '../../hooks/Tokens'
-import { useStakingContract } from '../../hooks/useContract'
 import { useCombinedActiveList } from '../../state/lists/hooks'
-import { useTransactionAdder } from '../../state/transactions/hooks'
 
 const DEFAULT_START_DATE = new Date()
 DEFAULT_START_DATE.setDate(DEFAULT_START_DATE.getDate() + 1)
@@ -15,28 +14,28 @@ const DEFAULT_END_DATE = new Date()
 DEFAULT_END_DATE.setDate(DEFAULT_END_DATE.getDate() + 7)
 
 export default function Incentive() {
-  const [token, setToken] = useState<string>('')
-  const stakingContract = useStakingContract(true)
+  const [pool, setPool] = useState<string>('')
+  const [rewardToken, setRewardToken] = useState<string>('')
+  const { createIncentive } = useStaking()
+  const [amount, setAmount] = useState<number>(0)
 
   const tokens = useCombinedActiveList()
-  const allTokens = useAllTokens()  
+  const allTokens = useAllTokens()
   const tokenComparator = useTokenComparator()
 
   const sortedTokens: Token[] = useMemo(() => {
     return Object.values(allTokens).sort(tokenComparator)
   }, [allTokens, tokenComparator])
- 
+
   const [startDate, setStartDate] = useState<Date>(DEFAULT_START_DATE)
   const [endDate, setEndDate] = useState<Date>(DEFAULT_END_DATE)
 
-
-  function toUnix(date: Date): string {
-    return (date.getTime() / 1000).toFixed().toString()
+  function toUnix(date: Date): number {
+    return parseInt((date.getTime() / 1000).toFixed())
   }
 
-
   const onSubmit = async () => {
-    stakingContract
+    createIncentive(pool, rewardToken, amount, toUnix(startDate), toUnix(endDate))
   }
 
   return (
@@ -44,44 +43,46 @@ export default function Incentive() {
       <NoSSR>
         <MetaMaskCard />
       </NoSSR>
-
       <h1>Create Incentive</h1>
       Pool:
       <div>
-      <select onChange={(e) => setToken(e.target.value)}>
-      {[['DAI/WETH','address']].map((value, i) => (<option value={value[1]} key={i}>{value[0]}</option> ))}
-      </select>
-    </div>
-      Token:
-    <div>
-    <select onChange={(e) => setToken(e.target.value)}>
-      {sortedTokens.map((token, i) => (<option value={token.address} key={i}>{token.symbol}</option> ))}
-      </select>
-    </div>
-  
-    <div>
-      Amount:
-     <div>
-     <input type="number"></input>
+        <select onChange={(e) => setRewardToken(e.target.value)}>
+          {[['DAI/WETH', 'address']].map((value, i) => (
+            <option value={value[1]} key={i}>
+              {value[0]}
+            </option>
+          ))}
+        </select>
       </div>
-
+      Reward Token:
       <div>
+        <select onChange={(e) => setRewardToken(e.target.value)}>
+          {sortedTokens.map((token, i) => (
+            <option value={token.address} key={i}>
+              {token.symbol}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        Amount:
+        <div>
+          <input type="number" onChange={(e) => setAmount(parseInt(e.target.value))}></input>
+        </div>
+        <div>
           <label>Start: </label>
           <div>
-
-          <input type="datetime-local" onChange={(e) => setStartDate(new Date(e.target.value))}></input>
+            <input type="datetime-local" onChange={(e) => setStartDate(new Date(e.target.value))}></input>
           </div>
         </div>
         <div>
           <label>End: </label>
           <div>
-
-          <input type="datetime-local" onChange={(e) => setEndDate(new Date(e.target.value))}></input>
+            <input type="datetime-local" onChange={(e) => setEndDate(new Date(e.target.value))}></input>
           </div>
         </div>
-    <button onClick={onSubmit}>Send reward</button>
-
-    </div>
+        <button onClick={onSubmit}>Send reward</button>
+      </div>
     </div>
   )
 }
