b03c5d40d8fa14de22cd4b4c5d148e627d4d1a25
OlaStenberg <hello@olastenberg.com>
2022-03-18 13:47:08 +0100

    refactor: add safemanager

---
+++ apps/dao/lib/safemanager.ts
@@ -0,0 +1,38 @@
+import { SafeInfo } from '@gnosis.pm/safe-react-gateway-sdk'
+import { ChainId, safes } from '../constants'
+
+export const getSafes = (): Promise<SafeInfo[]> =>
+  Promise.all(
+    safes.map(({ baseUrl, chainId, address }) =>
+      fetch(
+        chainId !== ChainId.HARMONY ? `${baseUrl}/chains/${chainId}/safes/${address}` : `${baseUrl}/safes/${address}`,
+      ).then((response) =>
+        response.json().then((data) => {
+          updateFields(data, chainId)
+          return data as SafeInfo
+        }),
+      ),
+    ),
+  )
+
+/**
+ * Some gnosis network have different APIs, this function maps those fields to make the data compatible with @type {SafeInfo}
+ * @param data json response
+ * @param chainId
+ */
+function updateFields(data: any, chainId: ChainId) {
+  if (!data.address?.value) {
+    data.address = { value: data.address }
+  }
+
+  if (!data.owners[0]?.value) {
+    const ownersValue = data.owners.map((owner) => {
+      return { value: owner }
+    })
+    data.owners = ownersValue
+  }
+
+  if (!data.chainId) {
+    data.chainId = chainId
+  }
+}
--- apps/dao/pages/safes.tsx
+++ apps/dao/pages/safes.tsx
@@ -1,42 +1,18 @@
 import { AddressEx, SafeInfo } from '@gnosis.pm/safe-react-gateway-sdk'
-import fetch from 'isomorphic-unfetch'
 import { FC } from 'react'
 import useSWR from 'swr'
-import { ChainId, safes, users } from '../constants'
-
-const getSafes = (): Promise<SafeInfo[]> =>
-  Promise.all(
-    safes.map(({ baseUrl, chainId, address }) =>
-      fetch(
-        chainId !== ChainId.HARMONY ? `${baseUrl}/chains/${chainId}/safes/${address}` : `${baseUrl}/safes/${address}`,
-      ).then((response) =>
-        response.json().then((data) => {
-          if (!data.address?.value) {
-            data.address = { value: data.address }
-          }
-
-          if (!data.owners[0]?.value) {
-            const ownersValue = data.owners.map((owner) => {
-              return { value: owner }
-            })
-            data.owners = ownersValue
-          }
-
-          if (!data.chainId) {
-            data.chainId = chainId
-          }
-          return data as SafeInfo
-        }),
-      ),
-    ),
-  )
+import { ChainId, users } from '../constants'
+import { getSafes } from '../lib/safemanager'
 
 interface SafesProps {
   safes: SafeInfo[]
 }
 
 const getUsers = (owners: AddressEx[]) => {
-  return owners.map((owner) => users.get(owner.value) ?? owner.value).join(' ')
+  return owners
+    .map((owner) => users.get(owner.value) ?? owner.value)
+    .sort()
+    .join(' ')
 }
 
 const Safes: FC<SafesProps> = ({ safes }) => {
@@ -47,7 +23,7 @@ const Safes: FC<SafesProps> = ({ safes }) => {
       <h1>Multisigs</h1>
       {safes.map((safe, i) => (
         <div key={`${safe.chainId}-${safe.address}${i}`}>
-          {ChainId[safe.chainId]} {safe.address.value} {safe.threshold} {getUsers(safe.owners)}
+          {ChainId[safe.chainId]}      {safe.address.value}       {safe.threshold}        {getUsers(safe.owners)}
         </div>
       ))}
     </>
@@ -58,8 +34,6 @@ export default Safes
 
 export const getStaticProps = async () => {
   const safes = await getSafes()
-
-  console.log({ safes })
 
   return {
     props: {
